834c710b2633f1169ba9ecc2f06f97c4dabc83a7
Ivan <Sipatov.Ivan@yandex.ru>
2022-07-19 17:07:59 +0300

    V1.1

---
+++ Task_8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int receptionTime = 10;
+            int hour = 60;
+            Console.WriteLine("Пожалуйста, введите число людей в очереди!");
+            int queue = (Convert.ToInt16(Console.ReadLine())) * receptionTime;
+            int waitingTimeHour = queue / hour;
+            int waitingTimeMinute = queue % hour;
+            Console.WriteLine("Вы должны отстоять в очереди: " + waitingTimeHour + " час(а,ов) " + waitingTimeMinute + " минут, приятного времяпровождения!");
+        }
+    }
+}
