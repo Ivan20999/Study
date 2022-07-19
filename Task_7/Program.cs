8e2a14a31097452a3095d2d9d62b370092132706
Ivan <Sipatov.Ivan@yandex.ru>
2022-07-19 16:41:31 +0300

    V1.1

---
+++ Task_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much gold do you have?");
+            int coins = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many crystals do you wish to purchase?");
+            int numberCrystals = Convert.ToInt32(Console.ReadLine());
+            int crystalPrice = 2;
+
+            if ((coins / crystalPrice) >= numberCrystals)
+            {
+                coins -= crystalPrice * numberCrystals;
+                Console.WriteLine("Congratulations! You have purchased: " + numberCrystals + " crystals. " + "There is gold left: " + coins + ".");
+            }
+            else
+            {
+                Console.WriteLine("Operation failed! We need more gold!");
+            }
+        }
+    }
+}
