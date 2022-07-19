cf617e9679b9ef5b5b61db78182e57309c613010
Ivan <Sipatov.Ivan@yandex.ru>
2022-07-19 13:16:39 +0300

    V1.1

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,9 +1,19 @@
-﻿using System;
-
-    internal class Program
+﻿
+internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        float damage = 0f;
+        decimal money = 0;
+        int amount = 0;
+        bool strike = false;
+        string unit = "kg";
+        float coordinate = 0f;
+        char mode = 'a';
+        double capacity = 0f;
+        byte data = 0;
+        object cube;
     }
+}
+
 
