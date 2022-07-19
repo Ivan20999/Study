5e5810885c95ca27cbe1cca0ac3c68831e45a89f
Ivan <Sipatov.Ivan@yandex.ru>
2022-07-19 14:15:46 +0300

    V1.0

---
+++ Tack_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        string name, gender, age, placeWork;
+        Console.WriteLine("Your name?");
+        name = Console.ReadLine();
+        Console.WriteLine("Your gender?");
+        gender = Console.ReadLine();
+        Console.WriteLine("Your age?");
+        age = Console.ReadLine();
+        Console.WriteLine("Your place of work?");
+        placeWork = Console.ReadLine();
+        Console.WriteLine("Вас зовут " + name + ", ваш пол " + gender + ", ваш возраст " + age + ", ваше место работы: " + placeWork + ".");
+    }
+    }
+
