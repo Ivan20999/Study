e3dd45c5ee0130fe48bf5166cae8dccce48a5f88
Ivan <Sipatov.Ivan@yandex.ru>
2022-07-19 16:43:35 +0300

    V1.2

--- Task_7/Program.cs
+++ Task_7/Program.cs
@@ -7,9 +7,9 @@ namespace Task_7
         static void Main(string[] args)
         {
             Console.WriteLine("How much gold do you have?");
-            int coins = Convert.ToInt32(Console.ReadLine());
+            int coins = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine("How many crystals do you wish to purchase?");
-            int numberCrystals = Convert.ToInt32(Console.ReadLine());
+            int numberCrystals = Convert.ToInt16(Console.ReadLine());
             int crystalPrice = 2;
 
             if ((coins / crystalPrice) >= numberCrystals)
