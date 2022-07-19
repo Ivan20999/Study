313ab7d9fed47bd683da614c9cec987de0e691ce
Ivan <Sipatov.Ivan@yandex.ru>
2022-07-19 15:07:14 +0300

    V1.2

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -4,7 +4,9 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        string name = "Oleg", surname = "Odegov";
+        string name = "Oleg";
+        string surname = "Odegov";
+
         Console.WriteLine("Before: " + name + " " + surname);
         (name, surname) = (surname, name);
         Console.WriteLine("After: " + name + " " + surname);
