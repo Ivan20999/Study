cce0107a6ab257ce323c95d792c634ce66c70d49
Ivan <Sipatov.Ivan@yandex.ru>
2022-07-19 14:32:20 +0300

    V1.0

---
+++ Task_5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Oleg", surname = "Odegov";
+        Console.WriteLine("Before: " + name + " " + surname);
+        (name, surname) = (surname, name);
+        Console.WriteLine("After: " + name + " " + surname);
+    }
+}
+
